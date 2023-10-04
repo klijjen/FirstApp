32788c23314ebf91c8a31e9c9609e74889d96b78
klijjen <luydofa_04@icloud.com>
2023-10-04 20:52:57 +0300

    Initial Commit

---
+++ Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Poor
+//
+//  Created by Лена Людофа on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
